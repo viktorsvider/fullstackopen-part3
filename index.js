1511d953b39cc72702f50d92f67c8b1bb8ed8a6e
Viktor Svider <viktor1svider@gmail.com>
2023-09-11 23:30:36 +0300

    add 3.7

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const morgan = require("morgan");
+
 const app = express();
+
 app.use(express.json());
+app.use(morgan("tiny"));
 
 let persons = [
   {
@@ -54,9 +58,10 @@ app.delete("/api/persons/:id", (request, response) => {
   const elementWithId = persons.find((person) => person.id === id);
   if (elementWithId === undefined) {
     response.status(404).end();
+  } else {
+    persons = persons.filter((persons) => persons.id !== id);
+    response.status(204).end();
   }
-  persons = persons.filter((persons) => persons.id !== id);
-  response.status(204).end();
 });
 
 app.get("/api/persons/:id", (request, response) => {
